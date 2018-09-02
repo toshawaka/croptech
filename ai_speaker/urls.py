4b589283cfc67520186fd6cb3f7ad9d89d5e8e4f
toshawaka <toshiya.wakatabe13@gmail.com>
2018-09-02 22:37:05 +0900

    first commit

--- ai_speaker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('say_hello_world/', views.say_hello_world, name='say_hello_world')
-]
